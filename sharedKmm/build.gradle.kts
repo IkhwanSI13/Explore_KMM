2c3652a5684bd683fd2174024ec621e05bc9488b
Ikhwan <aja.ikhwan@gmail.com>
2022-07-28 12:08:10 +0700

    progress

--- sharedKmm/build.gradle.kts
+++ sharedKmm/build.gradle.kts
@@ -14,8 +14,21 @@ kotlin {
             }
         }
     }
+
+    val ktorVersion = "1.4.0"
+    val serializationVersion = "1.0.0-RC"
+    val sqlDelightVersion: String by project
+    val coroutinesVersion = "1.3.9-native-mt"
+
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -25,6 +38,9 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 implementation("com.google.android.material:material:1.2.1")
+                implementation("com.squareup.picasso:picasso:2.71828")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
             }
         }
         val androidTest by getting {
@@ -33,7 +49,11 @@ kotlin {
                 implementation("junit:junit:4.13")
             }
         }
-        val iosMain by getting
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
+        }
         val iosTest by getting
     }
 }
@@ -52,7 +72,8 @@ val packForXcode by tasks.creating(Sync::class) {
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
     val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
     val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    val framework =
+        kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
     inputs.property("mode", mode)
     dependsOn(framework.linkTask)
     val targetDir = File(buildDir, "xcode-frameworks")
--- sharedKmm/src/commonMain/kotlin/id/yukngoding/explore_kmm/sharedKmm/Greeting.kt
+++ sharedKmm/src/commonMain/kotlin/id/yukngoding/explore_kmm/sharedKmm/Greeting.kt
@@ -1,6 +1,5 @@
 package id.yukngoding.explore_kmm.sharedKmm
 
-
 class Greeting {
     fun greeting(): String {
         return "Hello, ${Platform().platform}!"
